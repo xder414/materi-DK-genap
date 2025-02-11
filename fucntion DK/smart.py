578bf0bfb03dfe184653f985a066eb5fd3a86a3a
azrilrizkiansyah <aminahsiti5157@gmail.com>
2025-02-11 07:44:41 +0700

    aduh gantengnya

---
+++ fucntion DK/smart.py
@@ -0,0 +1,8 @@
+def cetakidentitassiswa():
+    print("Nama : Asep")
+    print("Kelas : 3A")
+    print("Jenis kelamin : Laki-laki")
+    
+#panggil fungsi cetak identitas siswa  
+cetakidentitassiswa()
+    
---
+++ fucntion DK/wellll2.py
@@ -0,0 +1,10 @@
+def cetakIndetitasSiswa(Nama,kelas,jenis_kelamin):
+    print("Nama : ",Nama)
+    print("kelas : ",kelas)
+    print("jenis_kelamin ",jenis_kelamin)
+    
+# panggil fungsi cetak identitas siswa 
+cetakIdentitasSiswa("Asep", "X-PPL,","Laki-Laki")
+cetakIndetitasSiswa("Dimas","X-PPL","Laki-Laki")
+cetakIdentitasSiswa("Haris","X-PPL","Laki-Laki")
+cetakIdentitasSiswa("ambatukam","X-PPL","Laki-Laki")
